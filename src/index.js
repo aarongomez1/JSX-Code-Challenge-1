95a326f81770ed69eb6babe3943da5f7394fc42f
Aaron Gomez <aaron.gomez1@gmail.com>
2020-08-31 17:54:38 -0700

    Performed the challenge

--- src/index.js
+++ src/index.js
@@ -2,3 +2,17 @@
 //It should display a h1 heading.
 //It should display an unordered list (bullet points).
 //It should contain 3 list elements.
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <div>
+    <h1>The Metal!</h1>
+    <ul>
+      <li>Satyricon</li>
+      <li>Cannibal Corpse</li>
+      <li>Danzig</li>
+    </ul>
+  </div>,
+  document.getElementById("root")
+);
